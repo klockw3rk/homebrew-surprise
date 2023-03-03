d99f91ef08ef6b915f20978965b303dc51707e08
klockw3rk <kw@localhost.localdomain>
2023-03-03 16:45:39 -0500

    added formula

---
+++ surprise.rb
@@ -0,0 +1,18 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Surprise < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/klockw3rk/merrychristmas/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "f2aa5952190dc42f2f2c62728a377ff90692fab7e16898f1ac45925ed2ca59fb"
+  license ""
+
+  def install
+	bin.install "surprise"
+  end
+
+  test do
+ 
+  end
+end
